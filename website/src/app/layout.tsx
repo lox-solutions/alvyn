ca3224e586431acd4df6c9bbf165109ed0b0e465
cpoyatos <dev@cpoyatos.com>
2026-06-16 15:21:12 +0200

    chore: add Footer component and integrate it into layouts

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -46,9 +46,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <Provider>
-          {children}
-        </Provider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
